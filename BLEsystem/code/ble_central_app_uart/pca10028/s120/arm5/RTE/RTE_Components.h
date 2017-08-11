960f1bcaf3abace47293d4315db0ba4abd2e316d
LabGrunt <johnson.thie@sydney.edu.au>
2017-08-11 11:00:19 +1000

    Added the transmitter and receiver code of the BLE system.

---
+++ BLEsystem/code/ble_central_app_uart/pca10028/s120/arm5/RTE/RTE_Components.h
@@ -0,0 +1,28 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_uart_c_s120_pca10028' 
+ * Target:  'nrf51422_xxac_s120' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define BSP_UART_SUPPORT
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define S120
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */
---
+++ BLEsystem/code/ble_central_app_uart/pca10031/s120/arm5/_build/quicksort.d
@@ -0,0 +1,3 @@
+.\_build\quicksort.o: ..\..\..\quicksort.c
+.\_build\quicksort.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\quicksort.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
